4255ca9898e9648c577c6ced570101be999e56d7
alex800121 <alex800121@hotmail.com>
2023-04-07 01:11:17 +0800

    Refactor asus config

---
+++ programs/onedrive/default.nix
@@ -0,0 +1,36 @@
+{ pkgs, userName, ... }: {
+  home.packages = [ pkgs.onedrive ];
+
+  xdg.configFile = {
+    onedrive = {
+      recursive = true;
+      source = ./.;
+    };
+  };
+
+  systemd.user.services."onedrive" = {
+    Unit.Description = "Onedrive sync service";
+    Service = {
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.onedrive}/bin/onedrive --monitor --confdir=/home/${userName}/.config/onedrive
+      '';
+      Restart = "on-failure";
+      RestartSec = "3s";
+      RestartPreventExitStatus=3;
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+    # script = ''
+    #   ${pkgs.onedrive}/bin/onedrive --monitor --confdir=/home/${userName}/.config/onedrive
+    # '';
+    # serviceConfig = {
+    #   Type = "simple";
+    #   Restart = "on-failure";
+    #   RestartSec = 3;
+    # };
+    # description = "Onedrive sync service";
+    # wantedBy = [ "default.target" ];
+  };
+}
