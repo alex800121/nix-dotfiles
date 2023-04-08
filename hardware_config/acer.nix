924431146109018ba4f82db54b185c5e3417b7b8
alex800121 <96431067+alex800121@users.noreply.github.com>
2023-04-08 23:26:05 +0800

    Update acer.nix

--- hardware_config/acer.nix
+++ hardware_config/acer.nix
@@ -26,6 +26,12 @@
   swapDevices =
     [ { device = "/dev/disk/by-uuid/094b5bb8-7f60-4241-ab50-3dba483b636d"; }
     ];
+   
+  fileSystems."/media/alex800121/Acer" = {
+    device = "/dev/disk/by-uuid/F2D200EBD200B63F";
+    fsType = "ntfs";
+    options = [ "rw" "uid=1000" ];
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
