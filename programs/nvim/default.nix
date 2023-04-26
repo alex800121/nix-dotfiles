c7359240fbf3bda88e5c245c640992caa39d45a1
alex800121 <alex800121@hotmail.com>
2023-04-26 16:22:29 +0800

    Remove old nvim configs

--- programs/nvim/default.nix
+++ programs/nvim/default.nix
@@ -2,51 +2,17 @@
   programs.neovim = {
     enable = true;
     plugins = ( with pkgs.vimPlugins; [
-      smart-splits-nvim
-      dracula-vim
-      tokyonight-nvim
-      popup-nvim
-      gruvbox-nvim
-      plenary-nvim
-      comment-nvim
-      nvim-web-devicons
-      nvim-tree-lua
-      bufferline-nvim
-      vim-bbye
-      lualine-nvim
-      toggleterm-nvim
-      nvim-cmp
-      cmp-nvim-lua
-      cmp-nvim-lsp
-      cmp-buffer
-      cmp-path
-      cmp-cmdline
-      cmp_luasnip
-      luasnip
-      friendly-snippets
-      nvim-lspconfig
-      telescope-nvim
-      playground
-      nvim-ts-rainbow
-      smart-splits-nvim
-      ( nvim-treesitter.withPlugins (plugins: with pkgs.tree-sitter-grammars; [
-        tree-sitter-c
-        tree-sitter-haskell
-        tree-sitter-nix
-        tree-sitter-lua
-        tree-sitter-rust
-      ] ) )
     ] );
     extraPackages = ( with pkgs; [
-      rnix-lsp
-      fd
     ] );
-    extraConfig = "luafile $HOME/.config/nvim/init_lua.lua";
+    extraLuaConfig = ''
+      print("Hello")
+    '';
   };
   xdg.configFile = {
     nvim = {
       recursive = true;
-      source = ./.;
+      source = ./luaConfig;
     };
   };
 }
