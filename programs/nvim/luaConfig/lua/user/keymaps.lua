e6f2ff51b86f0377103732f6c6f810c7330dae92
alex800121 <alex800121@hotmail.com>
2023-04-30 11:00:58 +0800

    tuning nvim options and keymaps

--- programs/nvim/luaConfig/lua/user/keymaps.lua
+++ programs/nvim/luaConfig/lua/user/keymaps.lua
@@ -1,24 +1,4 @@
-local maps = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
-maps("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-maps('n', 'x', '"_x', opts)
-maps('n', 'X', '"_X', opts)
-
-maps('n', 'L', ':bnext<CR>', opts)
-maps('n', 'H', ':bprevious<CR>', opts)
-maps('n', '<leader>x', ':Bdelete<CR>', opts)
-maps('n', '<leader>q', ':qa<CR>', opts)
-
-maps('v', '>', '>gv', opts)
-maps('v', '<', '<gv', opts)
-
-
-maps('n', '<leader>f', ':Telescope find_files<CR>', opts)
-maps('n', '<leader>g', ':Telescope live_grep<CR>', opts)
-
-maps('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
-maps('n', '<leader>p', ':NvimTreeToggle<CR>', opts)
+vim.keymap.set({ "n", "v", "o" }, "<SPACE>", "<Nop>", { noremap = true, buffer = false });
+vim.o.mapleader = " ";
+vim.keymap.set("n", "L", ":bnext<CR>", { noremap = true, buffer = false });
+vim.keymap.set("n", "H", ":bprevious<CR>", { noremap = true, buffer = false });
--- programs/nvim/luaConfig/lua/user/options.lua
+++ programs/nvim/luaConfig/lua/user/options.lua
@@ -1,43 +1,4 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-vim.opt.wrap = false
-vim.opt.signcolumn = "yes"
-vim.opt.syntax = "ON"
-vim.opt.background = "dark"
-vim.opt.termguicolors = true
-vim.opt.cursorline = true
-vim.opt.cursorlineopt = "both"
-vim.opt.guicursor = ""
+vim.opt.ruler = true;
 
-vim.opt.swapfile = false
-vim.opt.encoding = "utf8"
-vim.opt.fileencoding = "utf8"
-vim.opt.undofile = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.tabstop = 2
-vim.opt.completeopt = { "menuone", "noselect" }
-vim.opt.clipboard = "unnamedplus"
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.cmdheight = 1
-vim.opt.conceallevel = 0
-vim.opt.mouse = "a"
-vim.opt.pumheight = 10
-vim.opt.showtabline = 2
-vim.opt.updatetime = 300
-vim.cmd [[let netrw_browser_split=0]]
-vim.cmd [[colorscheme gruvbox]]
+vim.opt.number = true;
+vim.opt.relativenumber = true;
