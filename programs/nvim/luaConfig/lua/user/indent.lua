92e309468e59a93399a766657ac9e146a54a58dc
alex800121 <alex800121@hotmail.com>
2023-05-21 15:48:05 +0800

    Start Zellij config

--- programs/nvim/luaConfig/lua/user/indent.lua
+++ programs/nvim/luaConfig/lua/user/indent.lua
@@ -1,7 +1,7 @@
 local indentline = require'indent_blankline'
 
 vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
 indentline.setup({
