a7b43f4510a40b974df344200c22f5abd71a9a74
alex800121 <alex800121@hotmail.com>
2023-06-15 06:28:56 +0800

    Fix flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    # nixpkgsStable.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
