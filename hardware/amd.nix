7034c06fc541ae6638576a1579d2c2574b0231e0
alex800121 <alex800121@hotmail.com>
2024-01-17 12:08:36 +0800

    amd vlk

--- hardware/amd.nix
+++ hardware/amd.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
   hardware.amdgpu.loadInInitrd = true;
-  hardware.amdgpu.amdvlk = false;
+  hardware.amdgpu.amdvlk = true;
   hardware.amdgpu.opencl = true;
 }
