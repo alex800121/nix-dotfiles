8d2a7257be43f26c759c6de7864b4b53db206c8e
alex800121 <alex800121@hotmail.com>
2024-05-06 09:02:17 +0800

    wg-asus-nixos

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,9 +5,13 @@ let
   alexrpi4tp-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMnSnbIgHGRRSOQk1TtldRie2Hr9IPhsdX4eAskx1/jM";
   alexrpi4tp-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDYWWE+KePNOcBGSoBABUP8dZcUvJbWLGZtaoG0mrIeW";
   alexrpi4tp = [ alexrpi4tp-user alexrpi4tp-system ];
+  asus-nixos-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGf9DW0O5gkq0ephXxUl7SXgb6TMkAA7RgB9NIl4oKNi";
+  asus-nixos-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDotz0cHltq3gt5Iln5IKhPnj9PQbLp2iwi8KsnFc7Om";
+  asus-nixos = [ asus-nixos-user asus-nixos-system ];
 in
 {
   "ddtoken-acer-tp.age".publicKeys = acer-tp;
   "ddtoken-alexrpi4tp.age".publicKeys = alexrpi4tp;
   "wg-server-key.age".publicKeys = acer-tp;
+  "wg-client-key.age".publicKeys = asus-nixos;
 }
