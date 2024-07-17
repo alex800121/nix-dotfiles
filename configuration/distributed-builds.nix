84348b7f22b80c704e2df4f1bbcdacd38b6b9e02
alex800121 <alex800121@hotmail.com>
2024-07-17 20:26:09 +0800

    dist build

--- configuration/distributed-builds.nix
+++ configuration/distributed-builds.nix
@@ -4,14 +4,30 @@
     {
       hostName = "acer-tp";
       sshUser = "alex800121";
-      systems = ["x86_64-linux" "aarch64-linux"];
-      maxJobs = 3;
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      maxJobs = 4;
     }
     {
       hostName = "acer-nixos";
       sshUser = "alex800121";
-      systems = ["x86_64-linux" "aarch64-linux"];
-      maxJobs = 3;
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      maxJobs = 4;
+    }
+    {
+      hostName = "alexrpi4tp";
+      sshUser = "alex800121";
+      systems = [ "aarch64-linux" ];
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" "gccarch-armv8-a" ];
+      maxJobs = 2;
+    }
+    {
+      hostName = "alexrpi4dorm";
+      sshUser = "alex800121";
+      systems = [ "aarch64-linux" ];
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" "gccarch-armv8-a" ];
+      maxJobs = 1;
     }
   ];
 }
