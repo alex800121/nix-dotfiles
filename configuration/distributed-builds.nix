1f38dfdacbe14ccc393909b7013846eba1533ed8
alex800121 <alex800121@hotmail.com>
2024-07-20 06:50:30 +0800

    add more remote builds

--- configuration/distributed-builds.nix
+++ configuration/distributed-builds.nix
@@ -2,20 +2,6 @@
   nix.distributedBuilds = true;
   nix.buildMachines = [
     {
-      hostName = "acer-tp";
-      sshUser = "alex800121";
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-      maxJobs = 4;
-    }
-    {
-      hostName = "acer-nixos";
-      sshUser = "alex800121";
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-      maxJobs = 4;
-    }
-    {
       hostName = "alexrpi4tp";
       sshUser = "alex800121";
       systems = [ "aarch64-linux" ];
@@ -28,6 +14,20 @@
       systems = [ "aarch64-linux" ];
       supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" "gccarch-armv8-a" ];
       maxJobs = 1;
+    }
+    {
+      hostName = "acer-tp";
+      sshUser = "alex800121";
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      maxJobs = 4;
+    }
+    {
+      hostName = "acer-nixos";
+      sshUser = "alex800121";
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      maxJobs = 4;
     }
   ];
 }
