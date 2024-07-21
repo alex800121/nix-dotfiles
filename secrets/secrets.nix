a511f4a5effae3fda1fb2487c8f2212824641742
alex800121 <alex800121@hotmail.com>
2024-07-21 09:34:21 +0800

    all secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,9 +2,9 @@ let
   acer-tp-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9ecjWEa+jhCOrW4+RkxY0sW7AtsCmTNvdMbdbV/WjG";
   acer-tp-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM8QIkayntlJgarKb0rt5fy+413SfARjxszlE8tut4HB";
   acer-tp = [ acer-tp-user acer-tp-system ];
-  # acer-nixos-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9ecjWEa+jhCOrW4+RkxY0sW7AtsCmTNvdMbdbV/WjG";
-  # acer-nixos-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM8QIkayntlJgarKb0rt5fy+413SfARjxszlE8tut4HB";
-  # acer-nixos = [ acer-nixos-user acer-nixos-system ];
+  acer-nixos-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEydwYcKvthPPxPt4P7YkzUgzHahKk/gAMUv7py/jeCN";
+  acer-nixos-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL40ujSOBE4Nh6scsVCJv69893a+rfa2m9SDrAxfWG5P";
+  acer-nixos = [ acer-nixos-user acer-nixos-system ];
   alexrpi4tp-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMnSnbIgHGRRSOQk1TtldRie2Hr9IPhsdX4eAskx1/jM";
   alexrpi4tp-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDYWWE+KePNOcBGSoBABUP8dZcUvJbWLGZtaoG0mrIeW";
   alexrpi4tp = [ alexrpi4tp-user alexrpi4tp-system ];
@@ -27,6 +27,6 @@ in
   "nix-alexrpi4tp.age".publicKeys = alexrpi4tp;
   "nix-alexrpi4dorm.age".publicKeys = alexrpi4dorm;
   "nix-acer-tp.age".publicKeys = acer-tp;
-  # "nix-acer-nixos.age".publicKeys = acer-nixos;
+  "nix-acer-nixos.age".publicKeys = acer-nixos;
   # "wg-asus-nixos.age".publicKeys = asus-nixos;
 }
