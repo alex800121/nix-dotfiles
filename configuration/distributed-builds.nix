3afb93df1400ee36cb9e0d2e1f2c085207b0b09e
alex800121 <alex800121@hotmail.com>
2024-08-30 23:23:13 +0800

    try kernel boot

--- configuration/distributed-builds.nix
+++ configuration/distributed-builds.nix
@@ -11,8 +11,8 @@ in
 
   nix.sshServe.enable = true;
   nix.sshServe.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP6tNmqgOjEZxro8uX69HAerx1fInsBZlUjQCHPMwDxX alex800121@alexrpi4tp"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP+e3xAdX4FSnwAbB9FCl15mDOK72YgE5NJ6wX1sSfuE root@alexrpi4tp"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAQoJjXDwgMiNNzwMPmoZT8Di2At4FzNtUi5BojvItK/ alex800121@alexrpi4tp"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILoR3jzu0CtSYC/vYCyPnbHm7dlQNJRxGo4rWMtzL7J2 root@alexrpi4tp"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG885XYlPfi2h6hiokfhvZgHF1y2f3JnL11j+ARJkrXE alex800121@fw13"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKH5JQY6WjU7N0Z+WYoHiej4TzhN8Prfs5uiqvXExPvm root@fw13"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYRhvHSun0BXMV1oBi93FncWVFEma5pv6fKeruccOuW alex800121@acer-tp"
@@ -23,7 +23,7 @@ in
   nix.settings.trusted-substituters = [ "https://nix-community.cachix.org" ];
   nix.settings.trusted-users = [ "nix-ssh" "alex800121" "@wheel" ];
   nix.settings.trusted-public-keys = [
-    "nix-alexrpi4tp:u4ZxC4W4DBTP7/fiXuZcitkatNkdTKdzMSs9IbRZ5Aw=alex800121@alexrpi4tp"
+    "nix-alexrpi4tp:Gbm/vNeckoSLD8w/5/Ri1vecsgHRwqkxPCoAT2lAIWs="
     "nix-acer-tp:POOYVdWQp5avm9ZWd65SVcLdYiMNNx7Pfq/GtHr5WUc="
     "nix-fw13:yQd04YEg7RVa2KMfA8HtgYvTmki5BU0dVbJrRCLuEoU="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
