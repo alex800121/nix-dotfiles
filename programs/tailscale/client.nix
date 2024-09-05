53800f511055b6e9ff9345f019f2945fbe68128d
alex800121 <alex800121@hotmail.com>
2024-09-05 09:23:45 +0800

    exit node name

--- programs/tailscale/client.nix
+++ programs/tailscale/client.nix
@@ -3,7 +3,7 @@
   services.tailscale.openFirewall = true;
   services.tailscale.useRoutingFeatures = "client";
   services.tailscale.extraSetFlags = [
-    "--exit-node=100.111.136.66"
+    "--exit-node=acer-tp"
     "--exit-node-allow-lan-access=true"
   ];
 }
