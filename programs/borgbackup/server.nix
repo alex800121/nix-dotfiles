62f9d7ebfb859fc622f7ea65bec7a511790818a3
alex800121 <alex800121@hotmail.com>
2025-03-09 13:00:04 +0800

    borgbackup server config

--- programs/borgbackup/server.nix
+++ programs/borgbackup/server.nix
@@ -1,7 +1,11 @@
 { ... }: {
   services.borgbackup.repos."vaultwarden".path = "/var/lib/borgbackup/vaultwarden";
+  # services.borgbackup.repos."vaultwarden".user = "root";
+  # services.borgbackup.repos."vaultwarden".group = "root";
   services.borgbackup.repos."vaultwarden".quota = "50G";
   services.borgbackup.repos."vaultwarden".authorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOnZxTdSHinReC8qDH40sF6vTEmvNQei/OPfsxDtabLB root@alexrpi4tp"
   ];
+  services.borgbackup.repos."vaultwarden".allowSubRepos = false;
+  # services.borgbackup.repos."vaultwarden".allowSubRepos = true;
 }
