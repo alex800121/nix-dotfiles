d23b50987cee49f5921141113a6a0350fd1b7453
alex800121 <alex800121@hotmail.com>
2025-03-23 23:49:26 +0800

    update users

--- programs/borgbackup/vaultwarden.nix
+++ programs/borgbackup/vaultwarden.nix
@@ -8,24 +8,28 @@ let
   jobName = "vaultwarden";
   passphrase = "passphrase_borgbackup_vaultwarden_${hostName}";
   sshHostKey = "ssh_host_borgbackup_${serverName}_vaultwarden_${hostName}";
+  userName = config.users.users.vaultwarden.name;
+  groupName = config.users.groups.vaultwarden.name;
 in
 {
   # systemd.services."borgbackup-job-${jobName}".serviceConfig.SetCredentialEncrypted = setCred;
+  users.users.vaultwarden.createHome = true;
+  users.users.vaultwarden.home = "/home/vaultwarden";
   age.secrets.${passphrase} = {
     file = ../../secrets/${passphrase}.age;
-    owner = "root";
-    group = "root";
+    owner = userName;
+    group = groupName;
     mode = "600";
   };
   age.secrets."${sshHostKey}" = {
     file = ../../secrets/${sshHostKey}.age;
-    owner = "root";
-    group = "root";
+    owner = userName;
+    group = groupName;
     mode = "600";
   };
   services.borgbackup.jobs."${jobName}-${serverName}" = {
-    user = "root";
-    group = "root";
+    user = userName;
+    group = groupName;
     repo = "borg@${serverName}:.";
     paths = [ "/var/lib/vaultwarden" ];
     doInit = true;
