58960a9afb075035a1f21509bed4975f39556895
alex800121 <alex800121@hotmail.com>
2025-03-24 14:47:45 +0800

    fix caddy permissions

--- programs/vaultwarden/default.nix
+++ programs/vaultwarden/default.nix
@@ -2,6 +2,9 @@
 let
   inherit (config.networking) hostName;
   domainName = "alex${hostName}.duckdns.org";
+  port = config.services.mysql.settings.mysqld.port;
+  dataDir = config.services.mysql.settings.mysqld.datadir;
+  sslDir = config.security.acme.certs."${domainName}".directory;
   myCnfPath = "/var/lib/my.cnf";
   myCnfFile = pkgs.writeText "my.cnf" ''
     [galera]
@@ -16,8 +19,8 @@ let
     wsrep_sst_method=rsync
 
     [mysqld]
-    datadir=${config.services.mysql.settings.mysqld.datadir}
-    port=${builtins.toString config.services.mysql.settings.mysqld.port}
+    datadir=${dataDir}
+    port=${builtins.toString port}
   '';
 in
 {
@@ -26,7 +29,7 @@ in
   users.extraUsers."vaultwarden" = {
     isSystemUser = true;
     group = "vaultwarden";
-    extraGroups = [ config.security.acme.certs."${domainName}".group ];
+    # extraGroups = [ config.security.acme.certs."${domainName}".group ];
   };
 
   users.extraGroups."vaultwarden" = { };
@@ -46,23 +49,13 @@ in
     enable = true;
     package = pkgs.mariadb;
     configFile = myCnfPath;
-    # settings.galera = {
-    #   wsrep_provider = "${pkgs.mariadb-galera}/lib/galera/libgalera_smm.so";
-    #   wsrep_cluster_address = "gcomm://acer-tp,alexrpi4tp,oracle";
-    #   binlog_format = "ROW";
-    #   wsrep_on = "ON";
-    #   default_storage_engine = "InnoDB";
-    #   innodb_doublewrite = 1;
-    #   wsrep_cluster_name = "galera";
-    #   wsrep_node_address = userConfig.tsAddress;
-    #   wsrep_sst_method = "rsync";
-    # };
   };
   systemd.services.mysql-ip-set = {
     serviceConfig.type = "oneshot";
     requiredBy = [ "mysql.service" ];
     before = [ "mysql.service" ];
     requires = [ "tailscaled.service" ];
+    after = [ "tailscaled.service" ];
     path = with pkgs; [ tailscale gnused coreutils ];
     script = ''
       cp ${myCnfFile} ${myCnfPath}
@@ -89,12 +82,15 @@ in
   ];
 
   services.caddy.enable = true;
+  users.users."${config.services.caddy.user}".extraGroups = [ config.security.acme.certs."${domainName}".group ];
   services.caddy.virtualHosts."vaultwarden.${domainName}" = {
-    useACMEHost = domainName;
+    # useACMEHost = domainName;
     extraConfig = ''
+      tls ${sslDir}/cert.pem ${sslDir}/key.pem
       reverse_proxy http://localhost:${config.services.vaultwarden.config.ROCKET_PORT}
     '';
   };
+
   services.vaultwarden.enable = true;
   services.vaultwarden.environmentFile = config.age.secrets."vaultwarden.env".path;
   services.vaultwarden.backupDir = "/var/backup/vaultwarden";
