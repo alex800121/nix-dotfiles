92f85c4f477b5697e1b5aafbd248b70ab2a67922
alex800121 <alex800121@hotmail.com>
2025-03-27 12:49:49 +0800

    virtual ip?

--- programs/tailscale/server.nix
+++ programs/tailscale/server.nix
@@ -6,4 +6,8 @@
     "--accept-routes=true"
     "--advertise-routes=192.168.51.0/24,192.168.50.0/24"
   ];
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
 }
