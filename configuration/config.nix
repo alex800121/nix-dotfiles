ab6e575954640ee4a95f18bb6ee5a3034ce86da6
alex800121 <alex800121@hotmail.com>
2025-03-30 04:18:34 +0800

    adjust ip addr

--- configuration/config.nix
+++ configuration/config.nix
@@ -13,7 +13,7 @@
           clients = [ "alexrpi4tp" ];
         }
       ];
-      keepalived.routerIds = [ 1 2 3 ];
+      keepalived.routerIds = [ 4 2 3 ];
     };
     extraModules = [
       ./oracle.nix
@@ -49,7 +49,7 @@
       fontSize = 11.5;
       autoLogin = true;
       url = "alexrpi4gate";
-      keepalived.routerIds = [ 2 3 1 ];
+      keepalived.routerIds = [ 2 3 4 ];
     };
     extraModules = [
       ./alexrpi4tp.nix
@@ -74,7 +74,7 @@
           clients = [ "alexrpi4tp" ];
         }
       ];
-      keepalived.routerIds = [ 3 2 1 ];
+      keepalived.routerIds = [ 3 2 4 ];
     };
     extraModules = [
       ./acer-tp.nix
