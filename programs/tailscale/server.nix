fec44e4479cd75f29035564a1b837a28fb8138af
alex800121 <alex800121@hotmail.com>
2025-04-01 02:43:18 +0800

    fix default

--- programs/tailscale/server.nix
+++ programs/tailscale/server.nix
@@ -1,12 +1,10 @@
 { userConfit, pkgs, lib, ... }: {
-  imports = [./default.nix];
+  imports = [ ./default.nix ];
   services.tailscale.useRoutingFeatures = "server";
   services.tailscale.extraSetFlags = [
     "--advertise-exit-node"
     "--accept-routes=true"
   ];
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
-  };
+  boot.kernel.sysctl."net.ipv4.ip_forward" = lib.mkDefault 1;
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = lib.mkDefault 1;
 }
