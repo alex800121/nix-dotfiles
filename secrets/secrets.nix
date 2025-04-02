1c33780e839416997836bfcde96cee04fe85a31f
alex800121 <alex800121@hotmail.com>
2025-04-03 02:55:07 +0800

    add secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -26,6 +26,8 @@ in
   "ddtoken_oracle.age".publicKeys = oracle;
   "ddtoken_acer-tp.age".publicKeys = acer-tp;
   "ddtoken_fw13.age".publicKeys = fw13;
+  "tsapi_alexrpi4tp.age".publicKeys = alexrpi4tp;
+  "tsapi_oracle.age".publicKeys = oracle;
   "tsapi_acer-tp.age".publicKeys = acer-tp;
   "tsapi_fw13.age".publicKeys = fw13;
 }
