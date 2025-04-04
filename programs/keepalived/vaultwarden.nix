1956285e57da2dfb61a09ded1f09647add95b659
alex800121 <alex800121@hotmail.com>
2025-04-04 08:24:57 +0300

    custom keepalived config to fix extraConfig being after vrrpInstances

--- programs/keepalived/vaultwarden.nix
+++ programs/keepalived/vaultwarden.nix
@@ -43,105 +43,102 @@ let
     let
       ids = toString id;
     in
-    {
-      services.keepalived.vrrpInstances."VW_${ids}" = {
-        priority = initPrio - n;
-        state = "BACKUP";
-        # state = if n == 0 then "MASTER" else "BACKUP";
-        interface = name;
-        virtualIps = [
-          {
-            addr = "192.168.101.${ids}/32";
-            label = "${name}:vw${ids}";
-          }
-        ];
-        virtualRouterId = id;
-        extraConfig = ''
-          notify_master ${renewIp}
-          notify_backup ${renewIp}
-          track_process {
-            track_vaultwarden
-          }
-        '';
-      };
-    };
-  init = {
+    ''
+      vrrp_instance VW_${ids} {
+        state BACKUP
+        interface ${name}
+        track_process {
+          track_vaultwarden
+        }
+        virtual_router_id ${ids}
+        priority ${toString (initPrio - n)}
+        virtual_ipaddress {
+          192.168.101.${ids}/32 dev ${name} label vw${ids}
+        }
+        notify_master ${renewIp}
+        notify_backup ${renewIp}
+      }
 
-    age.secrets.tsApi = {
-      file = ../../secrets/tsapi_${hostName}.age;
-      owner = "root";
-      group = "root";
-      mode = "600";
-    };
-    environment.systemPackages = with pkgs; [
-      keepalived
-    ];
-    systemd.network.enable = true;
-    systemd.network.netdevs."20-${name}" = {
-      netdevConfig = {
-        Name = name;
-        Kind = "vxlan";
-      };
-      vxlanConfig = {
-        VNI = networkId;
-        Local = masterTsIp;
-        MacLearning = true;
-        DestinationPort = 4789;
-        Independent = true;
-      };
-    };
-    systemd.network.networks."20-${name}" = {
-      matchConfig = {
-        Name = name;
-      };
-      address = [
-        "${masterIp}/24"
-      ];
-      bridgeFDBs = map
-        (x: {
-          Destination = peerTsIp x;
-          VNI = networkId;
-          MACAddress = "00:00:00:00:00:00";
-        })
-        peers;
-    };
-    systemd.services.keepalived.postStop = updateScript;
-    systemd.services.keepalived.postStart = updateScript;
-    services.keepalived.enable = true;
-    services.keepalived.openFirewall = true;
-    services.keepalived.extraConfig = ''
+    '';
+  extraConfig =
+    ''
       vrrp_track_process track_vaultwarden {
         process vaultwarden
         quorum 1
         weight 10
         delay 1
       }
-    '';
-    services.keepalived.extraGlobalDefs = ''
-       # delay for second set of gratuitous ARPs after transition to MASTER
-      vrrp_garp_master_delay 10    # seconds, default 5, 0 for no second set
 
-      # number of gratuitous ARP messages to send at a time after transition to MASTER
-      vrrp_garp_master_repeat 1    # default 5
-
-      # delay for second set of gratuitous ARPs after lower priority advert received when MASTER
-      vrrp_garp_lower_prio_delay 10
-
-      # number of gratuitous ARP messages to send at a time after lower priority advert received when MASTER
-      vrrp_garp_lower_prio_repeat 1
-
-      # minimum time interval for refreshing gratuitous ARPs while MASTER
-      vrrp_garp_master_refresh 60  # secs, default 0 (no refreshing)
-
-      # number of gratuitous ARP messages to send at a time while MASTER
-      vrrp_garp_master_refresh_repeat 2 # default 1
-
-      # Delay in ms between gratuitous ARP messages sent on an interface
-      vrrp_garp_interval 0.001          # decimal, seconds (resolution usecs). Default 0.
-
-      # Delay in ms between unsolicited NA messages sent on an interface
-      vrrp_gna_interval 0.000001
-    '';
-  };
+    ''
+    + lib.concatStrings (lib.imap0 build routerIds);
 in
-foldl' recursiveUpdate init (imap0 build routerIds)
+{
+  age.secrets.tsApi = {
+    file = ../../secrets/tsapi_${hostName}.age;
+    owner = "root";
+    group = "root";
+    mode = "600";
+  };
+  environment.systemPackages = with pkgs; [
+    keepalived
+  ];
+  systemd.network.enable = true;
+  systemd.network.netdevs."20-${name}" = {
+    netdevConfig = {
+      Name = name;
+      Kind = "vxlan";
+    };
+    vxlanConfig = {
+      VNI = networkId;
+      Local = masterTsIp;
+      MacLearning = true;
+      DestinationPort = 4789;
+      Independent = true;
+    };
+  };
+  systemd.network.networks."20-${name}" = {
+    matchConfig = {
+      Name = name;
+    };
+    address = [
+      "${masterIp}/24"
+    ];
+    bridgeFDBs = map
+      (x: {
+        Destination = peerTsIp x;
+        VNI = networkId;
+        MACAddress = "00:00:00:00:00:00";
+      })
+      peers;
+  };
+  systemd.services.keepalived.postStop = updateScript;
+  systemd.services.keepalived.postStart = updateScript;
+  services.keepalived.enable = true;
+  services.keepalived.openFirewall = true;
+  services.keepalived.extraGlobalDefs = ''
+     # delay for second set of gratuitous ARPs after transition to MASTER
+    vrrp_garp_master_delay 10    # seconds, default 5, 0 for no second set
+
+    # number of gratuitous ARP messages to send at a time after transition to MASTER
+    vrrp_garp_master_repeat 1    # default 5
+
+    # delay for second set of gratuitous ARPs after lower priority advert received when MASTER
+    vrrp_garp_lower_prio_delay 10
+
+    # number of gratuitous ARP messages to send at a time after lower priority advert received when MASTER
+    vrrp_garp_lower_prio_repeat 1
+
+    # minimum time interval for refreshing gratuitous ARPs while MASTER
+    vrrp_garp_master_refresh 60  # secs, default 0 (no refreshing)
+
+    # number of gratuitous ARP messages to send at a time while MASTER
+    vrrp_garp_master_refresh_repeat 2 # default 1
+
+    # Delay in ms between gratuitous ARP messages sent on an interface
+    vrrp_garp_interval 0.001          # decimal, seconds (resolution usecs). Default 0.
+
+    # Delay in ms between unsolicited NA messages sent on an interface
+    vrrp_gna_interval 0.000001
+  '';
+  services.keepalived.extraConfig = extraConfig;
+}
