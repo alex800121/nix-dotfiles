2a1be3c070ac226ccf6a3a0dfd184e06e604959e
alex800121 <alex800121@hotmail.com>
2025-04-05 12:53:08 +0300

    fix keepalived peers

--- programs/keepalived/vaultwarden.nix
+++ programs/keepalived/vaultwarden.nix
@@ -5,8 +5,7 @@ let
   inherit (userConfig.tailscale) id peers;
   inherit (builtins) toString;
   inherit (lib) recursiveUpdate head tail map imap0 foldl';
-  master = head routers;
-  masterIp = "192.168.60.${toString master.id}";
+  masterIp = "192.168.60.${toString id}";
   masterTsIp = "100.64.0.${toString id}";
   peerTsIp = x: "100.64.0.${toString x}";
   initPrio = 100;
