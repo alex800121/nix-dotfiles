06d37445a954918d973ac99417faa8e16cf23b9a
alex800121 <alex800121@hotmail.com>
2025-05-24 16:02:46 +0800

    update minimal iron

--- configuration/oracle.nix
+++ configuration/oracle.nix
@@ -177,6 +177,4 @@ in
   services.openssh.enable = true;
 
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
-
--- programs/nvim/minimal/lua/user/iron.lua
+++ programs/nvim/minimal/lua/user/iron.lua
@@ -1,16 +1,11 @@
 local iron = require("iron.core")
+local view = require("iron.view")
 -- local ht = require("haskell-tools")
 
 iron.setup {
   config = {
     repl_definition = {
-      haskell = {
-        command = function(meta)
-          local file = vim.api.nvim_buf_get_name(meta.current_bufnr)
-          -- call `require` in case iron is set up before haskell-tools
-          return ht.repl.mk_repl_cmd(file)
-        end,
-      },
     },
+    repl_open_cmd = view.bottom(40),
   },
 }
