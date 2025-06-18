b7825601e8ea635757f9e2e85502f15d4bd26766
alex800121 <alex800121@hotmail.com>
2025-06-18 23:24:11 +0800

    delete redundant keys

--- configuration/distributed-builds.nix
+++ configuration/distributed-builds.nix
@@ -9,11 +9,6 @@ in
   nix.settings.trusted-substituters = [ "https://nix-community.cachix.org" ];
   nix.settings.trusted-users = [ "nix-ssh" "alex800121" "@wheel" ];
   nix.settings.trusted-public-keys = [
-    "nix-acer-tp:POOYVdWQp5avm9ZWd65SVcLdYiMNNx7Pfq/GtHr5WUc="
-    "nix-fw13:yQd04YEg7RVa2KMfA8HtgYvTmki5BU0dVbJrRCLuEoU="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "nix-alexrpi4tp:VvDq2N5Uq0wxLD1s78xD7odwk9zCSt8dqu6cdP5n8ws="
-    "nix-oracle:LzSqFMTgbYSaQ7+UKD7FmyclOi/iwh7GqVcyuETKsV0="
     "nix-common:tfkDg1lt8EUsog/Gex0wLDW61jqUslDg9nevljQ6aKM="
   ];
 
