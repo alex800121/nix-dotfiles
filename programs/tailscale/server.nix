7565d24fecfa585e4a4f4dab4bf070816678071b
alex800121 <alex800121@hotmail.com>
2025-06-18 14:59:41 +0800

    try tailscale set ip

--- programs/tailscale/server.nix
+++ programs/tailscale/server.nix
@@ -1,10 +1,60 @@
-{ userConfit, pkgs, lib, ... }: {
+{ userConfig, config, pkgs, lib, ... }:
+let
+  inherit (userConfig.tailscale) id;
+  tsIp = "100.64.0.${toString id}";
+  updateServerIp = ''
+    TS_API_TOKEN=$(cat ${config.age.secrets.tsServerApi.path})
+    TS_ID_IP=$(curl --request GET \
+                      --url https://api.tailscale.com/api/v2/tailnet/-/devices?fields=default \
+                      -u "$TS_API_TOKEN:" \
+                        | jq '.[].[] | select(.hostname=="oracle2") \
+                        | {id: .nodeId, addr: (.addresses.[] | select(test("100")))}')
+    TS_NODE_IP=$(echo $TS_ID_IP | jq -r .addr)
+    if [ "$TS_NODE_IP" != "${tsIp}" ]; then
+      TS_NODE_ID=$(echo $TS_ID_ID | jq -r .id)
+      POST_IP="{\"ipv4\":\"$TS_NODE_IP\"}"
+      curl/bin/curl --request POST \
+          --url https://api.tailscale.com/api/v2/device/$TS_NODE_ID/ip \
+          -u "$TS_API_TOKEN:" \
+          --header 'Content-Type: application/json' \
+          --data "$POST_IP"
+    fi
+    unset TS_API_TOKEN
+  '';
+in
+{
   imports = [ ./default.nix ];
   services.tailscale.useRoutingFeatures = "server";
   services.tailscale.extraSetFlags = [
     "--advertise-exit-node"
     "--accept-routes=true"
   ];
+
+  systemd.services."tailscale_server_ip" = {
+    enable = true;
+    description = "Set tailscale server ip";
+    wantedBy = [ "network-online.target" "tailscaled.service" ];
+    after = [ "network-online.target" "tailscaled.service" ];
+    path = with pkgs; [
+      coreutils
+      curl
+      jq
+    ];
+    script = updateServerIp;
+    serviceConfig = {
+      Type = "oneshot";
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+  };
+
   boot.kernel.sysctl."net.ipv4.ip_forward" = lib.mkDefault 1;
   boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = lib.mkDefault 1;
+  age.secrets.tsServerApi = {
+    file = ../../secrets/tsapi.age;
+    # file = ../../secrets/tsapi_${hostName}.age;
+    owner = "root";
+    group = "root";
+    mode = "600";
+  };
 }
