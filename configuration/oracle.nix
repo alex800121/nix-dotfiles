c5e0c5a0b71507687860ca8de9780b86af193b77
alex800121 <alex800121@hotmail.com>
2025-06-21 10:17:42 +0800

    generalize bash

--- configuration/oracle.nix
+++ configuration/oracle.nix
@@ -126,6 +126,45 @@ in
   time.timeZone = lib.mkDefault "Asia/Singapore";
   i18n.defaultLocale = "en_US.UTF-8";
 
+  programs.bash = {
+    completion.enable = true;
+    shellAliases = {
+      ls = "ls --color=auto";
+      ll = "ls -alh";
+      la = "ls -A";
+      l = "ls -CF";
+      nv = "nvim";
+    };
+    enableLsColors = true;
+    vteIntegration = true;
+    interactiveShellInit = ''
+      HISTCONTROL=ignorespace:ignoreboth
+      HISTSIZE=100000
+      HISTFILESIZE=100000
+      shopt -s histappend
+
+      shopt -s checkwinsize
+      shopt -s extglob
+      shopt -s globstar
+      shopt -s checkjobs
+      shopt -s cdspell
+      shopt -o -s vi
+    '';
+    promptInit = ''
+      # Provide a nice prompt if the terminal supports it.
+      if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+        USER_COLOR="1;31"
+        ((UID)) && USER_COLOR="1;32"
+        PS1="\n\[\e[\[$USER_COLOR\]m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w \$\[\e[0m\] "
+        if [ -z "$INSIDE_EMACS" ]; then
+          PS1="\[\e]0;\u@\h:\w\a\]$PS1"
+        fi
+        if test "$TERM" = "xterm"; then
+          PS1="\[\e]2;\h:\u:\w\007\]$PS1"
+        fi
+      fi
+    '';
+  };
   users.mutableUsers = true;
   users.users."${userName}" = {
     isNormalUser = true;
