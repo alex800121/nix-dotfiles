972ba5d85db235bfe9ea9ab3972bf8b1cdd5e9c6
alex800121 <alex800121@hotmail.com>
2025-11-28 21:34:08 +0800

    remove project.nvim

--- programs/nvim/luaConfig/init.lua
+++ programs/nvim/luaConfig/init.lua
@@ -6,7 +6,6 @@ require'user.options'
 require'user.keymaps'
 require'user.indent'
 require'user.buffer'
-require'user.project'
 require'user.nvimtree'.setup()
 -- require'user.treesitter'
 require'user.telescope'
